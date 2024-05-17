a1c431bbccdb94c339422eb60e946eace2e8c099
Grant Forrest <grant@butlr.io>
2024-05-17 12:52:27 -0400

    make cdk use app manifests

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -2,6 +2,7 @@
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { CdkStack } from '../lib/cdk-stack';
+import { apps } from '@biscuits/apps';
 
 const app = new cdk.App();
 
@@ -9,9 +10,35 @@ const common = {
   env: { account: '262002082180', region: 'us-east-1' },
 };
 
-for (const appId of ['www', 'blog', 'gnocchi', 'trip-tick']) {
-  new CdkStack(app, appId, {
+const allApps = [
+  ...apps,
+  {
+    id: 'www',
+    url: 'https://www.biscuits.club',
+    name: 'Biscuits Home',
+    description: 'The home page for Biscuits Club.',
+    iconPath: 'icon.png',
+    devOriginOverride: 'http://localhost:6123',
+    demoVideoSrc: '',
+    paidDescription: '',
+    paidFeatures: [],
+  },
+  {
+    id: 'blog',
+    url: 'https://blog.biscuits.club',
+    name: 'Biscuits Blog',
+    description: 'The blog for Biscuits Club.',
+    iconPath: 'icon.png',
+    devOriginOverride: 'http://localhost:3000',
+    demoVideoSrc: '',
+    paidDescription: '',
+    paidFeatures: [],
+  },
+];
+
+for (const appManifest of allApps) {
+  new CdkStack(app, appManifest.id, {
     ...common,
-    appId,
+    appId: appManifest.id,
   });
 }
