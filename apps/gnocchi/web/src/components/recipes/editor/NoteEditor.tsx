600db76704affa22e41d02552a5c101adfc5842c
Grant Forrest <gaforres@gmail.com>
2024-05-22 21:31:07 -0400

    remove note shadow

--- apps/gnocchi/web/src/components/recipes/editor/NoteEditor.tsx
+++ apps/gnocchi/web/src/components/recipes/editor/NoteEditor.tsx
@@ -3,30 +3,30 @@ import { Note } from '@a-type/ui/components/note';
 import { LiveUpdateTextField } from '@a-type/ui/components/liveUpdateTextField';
 
 export interface NoteEditorProps {
-	value: string;
-	onChange: (value: string) => void;
-	className?: string;
-	autoFocus?: boolean;
-	onBlur?: () => void;
+  value: string;
+  onChange: (value: string) => void;
+  className?: string;
+  autoFocus?: boolean;
+  onBlur?: () => void;
 }
 
 export function NoteEditor({
-	className,
-	autoFocus,
-	value,
-	onChange,
-	onBlur,
+  className,
+  autoFocus,
+  value,
+  onChange,
+  onBlur,
 }: NoteEditorProps) {
-	return (
-		<Note className={classNames('focus-within:shadow-focus', className)}>
-			<LiveUpdateTextField
-				className="border-none outline-none resize-none w-full rounded-none h-full p-0 m-0 [font-family:inherit] text-inherit [font-size:inherit] [font-style:inherit] bg-transparent focus:(outline-none bg-transparent border-transparent shadow-none)"
-				textArea
-				value={value}
-				onChange={onChange}
-				autoFocus={autoFocus}
-				onBlur={onBlur}
-			/>
-		</Note>
-	);
+  return (
+    <Note className={classNames('focus-within:shadow-focus', className)}>
+      <LiveUpdateTextField
+        className="border-none outline-none resize-none w-full rounded-none h-full p-0 m-0 [font-family:inherit] text-inherit [font-size:inherit] [font-style:inherit] bg-transparent shadow-none focus:(outline-none bg-transparent border-transparent shadow-none)"
+        textArea
+        value={value}
+        onChange={onChange}
+        autoFocus={autoFocus}
+        onBlur={onBlur}
+      />
+    </Note>
+  );
 }
