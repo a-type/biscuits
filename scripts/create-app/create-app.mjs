da84e2a34fba55a489295f36608c8c7814fc7abd
Grant Forrest <gaforres@gmail.com>
2024-05-31 15:04:21 -0400

    fix creds

--- scripts/create-app/create-app.mjs
+++ scripts/create-app/create-app.mjs
@@ -122,15 +122,15 @@ jobs:
           args: --follow-symlinks --delete
         env:
           AWS_S3_BUCKET: \${{ secrets.S3_BUCKET_${constify(appId)} }}
-          AWS_ACCESS_KEY_ID: \${{ secrets.AWS_ACCESS_KEY_ID }}
-          AWS_SECRET_ACCESS_KEY: \${{ secrets.AWS_SECRET_ACCESS_KEY }}
+          AWS_ACCESS_KEY_ID: \${{ secrets.DEPLOYER_AWS_ACCESS_KEY_ID }}
+          AWS_SECRET_ACCESS_KEY: \${{ secrets.DEPLOYER_AWS_SECRET_ACCESS_KEY }}
           SOURCE_DIR: './apps/${appId}/web/dist'
 
       - name: Invalidate CloudFront cache
         uses: chetan/invalidate-cloudfront-action@v2
         env:
-          AWS_ACCESS_KEY_ID: \${{ secrets.AWS_ACCESS_KEY_ID }}
-          AWS_SECRET_ACCESS_KEY: \${{ secrets.AWS_SECRET_ACCESS_KEY }}
+          AWS_ACCESS_KEY_ID: \${{ secrets.DEPLOYER_AWS_ACCESS_KEY_ID }}
+          AWS_SECRET_ACCESS_KEY: \${{ secrets.DEPLOYER_AWS_SECRET_ACCESS_KEY }}
           DISTRIBUTION: \${{ secrets.CLOUDFRONT_ID_${constify(appId)} }}
           PATHS: '/*'
           AWS_REGION: 'us-east-1'
