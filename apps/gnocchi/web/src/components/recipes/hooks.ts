69bbc61bd618ec0f964a7b992b57101c2135f78f
Grant Forrest <gaforres@gmail.com>
2024-12-19 12:55:34 -0500

    update react lib, perf improvements

--- apps/gnocchi/web/src/components/recipes/hooks.ts
+++ apps/gnocchi/web/src/components/recipes/hooks.ts
@@ -1,28 +1,27 @@
-import {
-  ObjectEntity,
-  Recipe,
-  RecipeDestructured,
-  RecipeSession,
-} from '@gnocchi.biscuits/verdant';
+import { SESSION_TIMEOUT } from '@/components/recipes/constants.js';
 import { hooks } from '@/stores/groceries/index.js';
-import { AnyExtension, ExtensionConfig, useEditor } from '@tiptap/react';
+import { assert } from '@a-type/utils';
+import {
+	ObjectEntity,
+	Recipe,
+	RecipeDestructured,
+	RecipeSession,
+} from '@gnocchi.biscuits/verdant';
+import Link from '@tiptap/extension-link';
+import { AnyExtension, useEditor } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
 import { useCallback, useEffect, useRef } from 'react';
 import { createTiptapExtensions } from './editor/tiptapExtensions.js';
-import StarterKit from '@tiptap/starter-kit';
-import { SESSION_TIMEOUT } from '@/components/recipes/constants.js';
-import Link from '@tiptap/extension-link';
-import { assert } from '@a-type/utils';
 
 export function useRecipeFromSlugUrl(url: string) {
-  const slug = url.split('-').pop();
-  const recipe = hooks.useOneRecipe({
-    index: {
-      where: 'slug',
-      equals: slug,
-    },
-  });
-  hooks.useWatch(recipe);
-  return recipe;
+	const slug = url.split('-').pop();
+	const recipe = hooks.useOneRecipe({
+		index: {
+			where: 'slug',
+			equals: slug,
+		},
+	});
+	return recipe;
 }
 
 /**
@@ -30,140 +29,140 @@ export function useRecipeFromSlugUrl(url: string) {
  * then runs the action with that session.
  */
 export function useCookSessionAction(recipe: Recipe | null) {
-  const client = hooks.useClient();
-  return useCallback(
-    (action: (session: RecipeSession) => void) => {
-      if (!recipe) {
-        return;
-      }
-      let session = recipe.get('session');
-      if (!session || session.get('startedAt') < Date.now() - SESSION_TIMEOUT) {
-        client
-          .batch({ undoable: false })
-          .run(() => {
-            recipe.set('session', {
-              completedIngredients: [],
-              completedInstructions: [],
-              ingredientAssignments: {},
-              instructionAssignments: {},
-              startedAt: Date.now(),
-            });
-            recipe.set('cookCount', recipe.get('cookCount') + 1);
-            recipe.set('lastCookedAt', Date.now());
-            session = recipe.get('session');
-          })
-          .flush();
-      }
-      assert(session);
-      action(session);
-    },
-    [recipe, client],
-  );
+	const client = hooks.useClient();
+	return useCallback(
+		(action: (session: RecipeSession) => void) => {
+			if (!recipe) {
+				return;
+			}
+			let session = recipe.get('session');
+			if (!session || session.get('startedAt') < Date.now() - SESSION_TIMEOUT) {
+				client
+					.batch({ undoable: false })
+					.run(() => {
+						recipe.set('session', {
+							completedIngredients: [],
+							completedInstructions: [],
+							ingredientAssignments: {},
+							instructionAssignments: {},
+							startedAt: Date.now(),
+						});
+						recipe.set('cookCount', recipe.get('cookCount') + 1);
+						recipe.set('lastCookedAt', Date.now());
+						session = recipe.get('session');
+					})
+					.flush();
+			}
+			assert(session);
+			action(session);
+		},
+		[recipe, client],
+	);
 }
 
 export function useActiveCookingSession(recipe: Recipe) {
-  const { session } = hooks.useWatch(recipe);
-  if (!session) {
-    return null;
-  }
-  return isActiveCookingSession(session) ? session : null;
+	const { session } = hooks.useWatch(recipe);
+	if (!session) {
+		return null;
+	}
+	return isActiveCookingSession(session) ? session : null;
 }
 
 export function isActiveCookingSession(session: RecipeSession | null) {
-  if (!session) {
-    return false;
-  }
-  return session.get('startedAt') > Date.now() - SESSION_TIMEOUT;
+	if (!session) {
+		return false;
+	}
+	return session.get('startedAt') > Date.now() - SESSION_TIMEOUT;
 }
 
 export function useSyncedInstructionsEditor({
-  recipe,
-  readonly = false,
-  useBasicEditor = false,
+	recipe,
+	readonly = false,
+	useBasicEditor = false,
 }: {
-  recipe: Recipe;
-  readonly?: boolean;
-  useBasicEditor?: boolean;
+	recipe: Recipe;
+	readonly?: boolean;
+	useBasicEditor?: boolean;
 }) {
-  return useSyncedEditor(
-    recipe,
-    'instructions',
-    readonly,
-    createTiptapExtensions(recipe, useBasicEditor),
-  );
+	return useSyncedEditor(
+		recipe,
+		'instructions',
+		readonly,
+		createTiptapExtensions(recipe, useBasicEditor),
+	);
 }
 
 export function useSyncedPreludeEditor(recipe: Recipe, readonly = false) {
-  return useSyncedEditor(recipe, 'prelude', readonly, [StarterKit, Link]);
+	return useSyncedEditor(recipe, 'prelude', readonly, [StarterKit, Link]);
 }
 
 function useSyncedEditor(
-  recipe: Recipe,
-  fieldName: keyof RecipeDestructured,
-  readonly: boolean,
-  extensions: AnyExtension[],
+	recipe: Recipe,
+	fieldName: keyof RecipeDestructured,
+	readonly: boolean,
+	extensions: AnyExtension[],
 ) {
-  const live = hooks.useWatch(recipe);
-  const field = live[fieldName] as ObjectEntity<any, any>;
-  const updatingRef = useRef(false);
-  const update = useCallback(
-    (editor: any) => {
-      if (updatingRef.current) {
-        return;
-      }
+	const live = hooks.useWatch(recipe);
+	const field = live[fieldName] as ObjectEntity<any, any>;
+	const updatingRef = useRef(false);
+	const update = useCallback(
+		(editor: any) => {
+			if (updatingRef.current) {
+				return;
+			}
 
-      const newData = editor.getJSON();
-      const value = recipe.get(fieldName) as ObjectEntity<any, any> | null;
-      if (!value) {
-        recipe.set(fieldName, newData);
-      } else {
-        value.update(newData, {
-          merge: false,
-          replaceSubObjects: false,
-        });
-      }
-    },
-    [recipe],
-  );
+			const newData = editor.getJSON();
+			const value = recipe.get(fieldName) as ObjectEntity<any, any> | null;
+			if (!value) {
+				recipe.set(fieldName, newData);
+			} else {
+				value.update(newData, {
+					merge: false,
+					replaceSubObjects: false,
+				});
+			}
+		},
+		[recipe],
+	);
 
-  const editor = useEditor(
-    {
-      extensions,
-      content: field?.getSnapshot() ?? {
-        type: 'doc',
-        content: [],
-      },
-      editable: !readonly,
-      onUpdate: ({ editor }) => {
-        update(editor);
-      },
-    },
-    [field, update],
-  );
+	const editor = useEditor(
+		{
+			extensions,
+			content: field?.getSnapshot() ?? {
+				type: 'doc',
+				content: [],
+			},
+			editable: !readonly,
+			onUpdate: ({ editor }) => {
+				update(editor);
+			},
+		},
+		[field, update],
+	);
 
-  useEffect(() => {
-    if (editor && !editor.isDestroyed && field) {
-      updatingRef.current = true;
-      const { from, to } = editor.state.selection;
-      editor.commands.setContent(field.getSnapshot(), false);
-      editor.commands.setTextSelection({ from, to });
-      updatingRef.current = false;
-    }
+	useEffect(() => {
+		if (editor && !editor.isDestroyed && field) {
+			updatingRef.current = true;
+			const { from, to } = editor.state.selection;
+			editor.commands.setContent(field.getSnapshot(), false);
+			editor.commands.setTextSelection({ from, to });
+			updatingRef.current = false;
+		}
 
-    return field?.subscribe('changeDeep', (target, info) => {
-      if (!info.isLocal || target === field) {
-        updatingRef.current = true;
-        if (editor) {
-          const { from, to } = editor.state.selection;
-          editor.commands.setContent(field.getSnapshot(), false);
-          editor.commands?.setTextSelection({ from, to });
-        }
-        updatingRef.current = false;
-      }
-    });
-  }, [field, editor]);
+		return field?.subscribe('changeDeep', (target, info) => {
+			if (!info.isLocal || target === field) {
+				updatingRef.current = true;
+				if (editor) {
+					const { from, to } = editor.state.selection;
+					editor.commands.setContent(field.getSnapshot(), false);
+					editor.commands?.setTextSelection({ from, to });
+				}
+				updatingRef.current = false;
+			}
+		});
+	}, [field, editor]);
 
-  return editor;
+	return editor;
 }
 
 /**
@@ -171,18 +170,18 @@ function useSyncedEditor(
  * instructions, ingredients, or prelude.
  */
 export function useWatchChanges(recipe: Recipe) {
-  const { ingredients, instructions, prelude } = hooks.useWatch(recipe);
+	const { ingredients, instructions, prelude } = hooks.useWatch(recipe);
 
-  useEffect(() => {
-    const unsubs = new Array<() => void>();
-    const updateTime = () => {
-      recipe.set('updatedAt', Date.now());
-    };
-    unsubs.push(ingredients.subscribe('changeDeep', updateTime));
-    unsubs.push(instructions.subscribe('changeDeep', updateTime));
-    unsubs.push(prelude.subscribe('changeDeep', updateTime));
-    return () => {
-      unsubs.forEach((unsub) => unsub());
-    };
-  }, [ingredients, instructions, prelude]);
+	useEffect(() => {
+		const unsubs = new Array<() => void>();
+		const updateTime = () => {
+			recipe.set('updatedAt', Date.now());
+		};
+		unsubs.push(ingredients.subscribe('changeDeep', updateTime));
+		unsubs.push(instructions.subscribe('changeDeep', updateTime));
+		unsubs.push(prelude.subscribe('changeDeep', updateTime));
+		return () => {
+			unsubs.forEach((unsub) => unsub());
+		};
+	}, [ingredients, instructions, prelude]);
 }
