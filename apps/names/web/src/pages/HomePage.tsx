ab696b37c19ba6250ea3e4774093a945c0c0c654
Grant Forrest <gaforres@gmail.com>
2025-02-10 10:53:57 -0500

    install button

--- apps/names/web/src/pages/HomePage.tsx
+++ apps/names/web/src/pages/HomePage.tsx
@@ -1,20 +1,23 @@
 import { SuperBarSuggestions } from '@/components/superBar/SuperBarSuggestions.jsx';
 import { Box, HideWhileKeyboardOpen } from '@a-type/ui';
-import { AppIcon, UserMenu } from '@biscuits/client';
+import { AppIcon, InstallButton, UserMenu } from '@biscuits/client';
 
 export interface HomePageProps {}
 
 export function HomePage({}: HomePageProps) {
 	return (
 		<>
-			<Box items="center" justify="between" className="pb-sm">
+			<Box items="center" justify="between" className="pb-sm" asChild>
 				<HideWhileKeyboardOpen>
 					<Box items="center" gap="sm">
 						<AppIcon className="w-32px h-32px" />
 						<h1 className="font-fancy m-0 text-md font-semibold p-0">Names</h1>
 					</Box>
+					<Box items="center" gap="sm">
+						<InstallButton />
+						<UserMenu />
+					</Box>
 				</HideWhileKeyboardOpen>
-				<UserMenu />
 			</Box>
 			<SuperBarSuggestions />
 		</>
