cb2b3685d9404901f8d6d8310b09519abd6704b9
Grant Forrest <gaforres@gmail.com>
2025-03-11 22:19:00 -0400

    add post hub to cdk

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,67 +1,77 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { AppStack } from '../lib/AppStack.js';
 import { apps } from '@biscuits/apps';
+import * as cdk from 'aws-cdk-lib';
+import 'source-map-support/register';
+import { AppStack } from '../lib/AppStack.js';
 import { SSGStack } from '../lib/SSGStack.js';
 
 const app = new cdk.App();
 
 const common = {
-  env: { account: '262002082180', region: 'us-east-1' },
+	env: { account: '262002082180', region: 'us-east-1' },
 };
 
 const allApps = [
-  ...apps,
-  {
-    id: 'www',
-    url: 'https://www.biscuits.club',
-    name: 'Biscuits Home',
-    description: 'The home page for Biscuits Club.',
-    iconPath: 'icon.png',
-    devOriginOverride: 'http://localhost:6123',
-    demoVideoSrc: '',
-    paidDescription: '',
-    paidFeatures: [],
-  },
-  {
-    id: 'blog',
-    url: 'https://blog.biscuits.club',
-    name: 'Biscuits Blog',
-    description: 'The blog for Biscuits Club.',
-    iconPath: 'icon.png',
-    devOriginOverride: 'http://localhost:3000',
-    demoVideoSrc: '',
-    paidDescription: '',
-    paidFeatures: [],
-  },
+	...apps,
+	{
+		id: 'www',
+		url: 'https://www.biscuits.club',
+		name: 'Biscuits Home',
+		description: 'The home page for Biscuits Club.',
+		iconPath: 'icon.png',
+		devOriginOverride: 'http://localhost:6123',
+		demoVideoSrc: '',
+		paidDescription: '',
+		paidFeatures: [],
+	},
+	{
+		id: 'blog',
+		url: 'https://blog.biscuits.club',
+		name: 'Biscuits Blog',
+		description: 'The blog for Biscuits Club.',
+		iconPath: 'icon.png',
+		devOriginOverride: 'http://localhost:3000',
+		demoVideoSrc: '',
+		paidDescription: '',
+		paidFeatures: [],
+	},
 ];
 
 for (const appManifest of allApps) {
-  new AppStack(app, appManifest.id, {
-    ...common,
-    appId: appManifest.id,
-    tags: {
-      appId: appManifest.id,
-    },
-  });
+	new AppStack(app, appManifest.id, {
+		...common,
+		appId: appManifest.id,
+		tags: {
+			appId: appManifest.id,
+		},
+	});
 }
 
 const ssg = [
-  {
-    id: 'gnocchi-hub',
-    subdomain: 'recipes.gnocchi',
-    renderPath: '/gnocchi/hubRecipe',
-  },
+	{
+		id: 'gnocchi-hub',
+		subdomain: 'recipes.gnocchi',
+		renderPath: '/gnocchi/hubRecipe',
+	},
+	{
+		id: 'wishWash-hub',
+		subdomain: 'lists.wish-wash',
+		renderPath: '/wishWash/hubList',
+	},
+	{
+		id: 'post-hub',
+		subdomain: 'pub.post',
+		renderPath: '/post/hub',
+	},
 ];
 
 for (const ssgManifest of ssg) {
-  new SSGStack(app, ssgManifest.id, {
-    ...common,
-    subdomain: ssgManifest.subdomain,
-    renderPath: ssgManifest.renderPath,
-    tags: {
-      subdomain: ssgManifest.subdomain,
-    },
-  });
+	new SSGStack(app, ssgManifest.id, {
+		...common,
+		subdomain: ssgManifest.subdomain,
+		renderPath: ssgManifest.renderPath,
+		tags: {
+			subdomain: ssgManifest.subdomain,
+		},
+	});
 }
--- server/src/config/secrets.ts
+++ server/src/config/secrets.ts
@@ -6,26 +6,29 @@ export const SESSION_SECRET = process.env.SESSION_SECRET!;
 export const OPENWEATHER_API_KEY = process.env.OPENWEATHER_API_KEY!;
 export const GOOGLE_MAPS_API_KEY = process.env.GOOGLE_MAPS_API_KEY!;
 export const GNOCCHI_HUB_CLOUDFRONT_ID = process.env.GNOCCHI_HUB_CLOUDFRONT_ID;
+export const POST_HUB_CLOUDFRONT_ID = process.env.POST_HUB_CLOUDFRONT_ID;
+export const WISH_WASH_HUB_CLOUDFRONT_ID =
+	process.env.WISH_WASH_HUB_CLOUDFRONT_ID;
 
 const TEST = process.env.TEST;
 
 assert(
-  TEST || !!VERDANT_SECRET,
-  'VERDANT_SECRET environment variable must be set',
+	TEST || !!VERDANT_SECRET,
+	'VERDANT_SECRET environment variable must be set',
 );
 assert(
-  TEST || !!STRIPE_WEBHOOK_SECRET,
-  'STRIPE_WEBHOOK_SECRET environment variable must be set',
+	TEST || !!STRIPE_WEBHOOK_SECRET,
+	'STRIPE_WEBHOOK_SECRET environment variable must be set',
 );
 assert(
-  TEST || !!SESSION_SECRET,
-  'SESSION_SECRET environment variable must be set',
+	TEST || !!SESSION_SECRET,
+	'SESSION_SECRET environment variable must be set',
 );
 assert(
-  TEST || !!OPENWEATHER_API_KEY,
-  'OPENWEATHER_API_KEY environment variable must be set',
+	TEST || !!OPENWEATHER_API_KEY,
+	'OPENWEATHER_API_KEY environment variable must be set',
 );
 assert(
-  TEST || !!GOOGLE_MAPS_API_KEY,
-  'GOOGLE_MAPS_API_KEY environment variable must be set',
+	TEST || !!GOOGLE_MAPS_API_KEY,
+	'GOOGLE_MAPS_API_KEY environment variable must be set',
 );
