cb639a8a9ff3b5843619e000d0f7b5f31ace277d
Grant Forrest <gaforres@gmail.com>
2025-03-23 15:30:41 -0400

    missed one

--- scripts/create-app/create-app.mjs
+++ scripts/create-app/create-app.mjs
@@ -123,7 +123,6 @@ jobs:
         with:
           apiToken: \${{ secrets.CLOUDFLARE_API_TOKEN }}
           command: pages deploy dist --project-name=prod-biscuits-${appId}-app
-
           packageManager: pnpm
           workingDirectory: ./apps/${appId}/web
 `;
