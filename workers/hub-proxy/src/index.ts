34be9cd8cf6c7539655ea950e376d04510b58e28
Grant Forrest <gaforres@gmail.com>
2025-03-25 12:49:21 -0400

    wrap the whole thing

--- workers/hub-proxy/src/index.ts
+++ workers/hub-proxy/src/index.ts
@@ -12,33 +12,34 @@ interface Environment {
  */
 export default {
 	async fetch(request, env): Promise<Response> {
-		const incomingHost = request.headers.get('host');
-		if (!incomingHost) {
-			return new Response('No host header', {
-				status: 400,
-			});
-		}
-
-		const apiHost = new URL(env.API_ORIGIN).hostname;
-		// proxy request to API origin
-		const url = new URL(request.url);
-		url.hostname = apiHost;
-
-		const headers: HeadersInit = {
-			'x-forwarded-host': incomingHost,
-			'x-forwarded-proto': request.headers.get('x-forwarded-proto') || 'https',
-			'x-forwarded-for':
-				request.headers.get('x-forwarded-for') ||
-				request.headers.get('cf-connecting-ip') ||
-				request.headers.get('x-real-ip') ||
-				request.headers.get('x-forwarded-host') ||
-				'',
-			'x-forwarded-port': request.headers.get('x-forwarded-port') || '443',
-			'x-forwarded-path': request.headers.get('x-forwarded-path') || '/',
-			'x-forwarded-url': request.headers.get('x-forwarded-url') || '/',
-		};
-
 		try {
+			const incomingHost = request.headers.get('host');
+			if (!incomingHost) {
+				return new Response('No host header', {
+					status: 400,
+				});
+			}
+
+			const apiHost = new URL(env.API_ORIGIN).hostname;
+			// proxy request to API origin
+			const url = new URL(request.url);
+			url.hostname = apiHost;
+
+			const headers: HeadersInit = {
+				'x-forwarded-host': incomingHost,
+				'x-forwarded-proto':
+					request.headers.get('x-forwarded-proto') || 'https',
+				'x-forwarded-for':
+					request.headers.get('x-forwarded-for') ||
+					request.headers.get('cf-connecting-ip') ||
+					request.headers.get('x-real-ip') ||
+					request.headers.get('x-forwarded-host') ||
+					'',
+				'x-forwarded-port': request.headers.get('x-forwarded-port') || '443',
+				'x-forwarded-path': request.headers.get('x-forwarded-path') || '/',
+				'x-forwarded-url': request.headers.get('x-forwarded-url') || '/',
+			};
+
 			return await fetch(url, {
 				headers,
 			});
