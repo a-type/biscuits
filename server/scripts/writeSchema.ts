ba21c471ac08678eb10b297ad7800dc24563f6ce
Grant Forrest <gaforres@gmail.com>
2025-03-29 22:59:41 -0400

    autogenerate gql types

--- server/scripts/writeSchema.ts
+++ server/scripts/writeSchema.ts
@@ -1,3 +1,10 @@
+import { generateOutput } from '@gql.tada/cli-utils';
 import { writeSchema } from '../src/tasks/writeSchema.js';
+
 writeSchema();
+await generateOutput({
+	output: '../packages/graphql/src/graphql-env.d.ts',
+	disablePreprocessing: false,
+	tsconfig: '../packages/graphql/tsconfig.json',
+});
 console.log('Wrote schema to schema.graphql', new Date().toISOString());
