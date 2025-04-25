92acc2427f75e7d6de8d9fe29bed248fe0422ecf
Grant Forrest <gaforres@gmail.com>
2025-04-25 17:43:05 -0400

    add manage storage

--- apps/wish-wash/web/src/pages/SettingsPage.tsx
+++ apps/wish-wash/web/src/pages/SettingsPage.tsx
@@ -1,6 +1,11 @@
-import { H1, PageContent, PageRoot } from '@a-type/ui';
+import { Button, H1, Icon, PageContent, PageRoot } from '@a-type/ui';
 import { DarkModeToggle } from '@biscuits/client';
-import { UpdatePrompt, usePollForUpdates } from '@biscuits/client/apps';
+import {
+	ManageStorage,
+	UpdatePrompt,
+	usePollForUpdates,
+} from '@biscuits/client/apps';
+import { Link } from '@verdant-web/react-router';
 
 export interface SettingsPageProps {}
 
@@ -11,10 +16,17 @@ export function SettingsPage({}: SettingsPageProps) {
 		<PageRoot>
 			<PageContent fullHeight noPadding>
 				<div className="flex flex-col w-full mt-6 p-4 gap-4 items-start">
+					<Button asChild color="ghost">
+						<Link to="/">
+							<Icon name="arrowLeft" />
+							Back
+						</Link>
+					</Button>
 					<H1>Settings</H1>
 					<UpdatePrompt />
 					<div className="flex flex-col items-start w-full gap-4">
 						<DarkModeToggle />
+						<ManageStorage />
 					</div>
 				</div>
 			</PageContent>
