75ac14c3049bf6037d2ed52d0e2251830999be2c
Grant Forrest <gaforres@gmail.com>
2025-04-26 20:32:57 -0400

    bypass pwa-install on chrome and edge

--- packages/client/src/apps/InstallButton.tsx
+++ packages/client/src/apps/InstallButton.tsx
@@ -1,21 +1,27 @@
 import { Button, ButtonProps, Icon } from '@a-type/ui';
 import '@khmyznikov/pwa-install';
 import { useSnapshot } from 'valtio';
-import { installState } from '../install.js';
+import { installState, triggerInstall } from '../install.js';
+import { getIsFirefox, getIsSafari, getOS } from '../platform.js';
 import { PwaInstaller } from './PwaInstaller.js';
 
 export function InstallButton(props: ButtonProps) {
 	const { installReady } = useSnapshot(installState);
 
-	const show = () => {
-		PwaInstaller.show();
+	const onClick = () => {
+		const os = getOS();
+		if (getIsSafari() || getIsFirefox() || os === 'iOS' || os === 'Mac OS') {
+			PwaInstaller.show();
+		} else {
+			triggerInstall();
+		}
 	};
 
 	if (!installReady) return null;
 
 	return (
 		<>
-			<Button color="ghost" size="small" onClick={show} {...props}>
+			<Button color="ghost" size="small" onClick={onClick} {...props}>
 				{props.children || (
 					<>
 						<Icon name="arrowDown" />
