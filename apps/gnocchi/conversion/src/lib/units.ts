7e44125011d1616dae55c9073814d6b7f89b133c
Grant Forrest <gaforres@gmail.com>
2025-11-26 18:05:56 -0500

    ignore more

--- apps/gnocchi/conversion/src/lib/units.ts
+++ apps/gnocchi/conversion/src/lib/units.ts
@@ -1,36 +1,39 @@
 import configureMeasurements, {
-  length,
-  mass,
-  pieces,
-  UnitDescription,
-  volume,
+	length,
+	mass,
+	pieces,
+	UnitDescription,
+	volume,
 } from 'convert-units';
 
-// @ts-ignore
 export const convert = configureMeasurements({
-  volume,
-  mass,
-  pieces,
-  length,
+	// @ts-ignore
+	volume,
+	// @ts-ignore
+	mass,
+	// @ts-ignore
+	pieces,
+	// @ts-ignore
+	length,
 });
 
 export function lookupUnit(unitName: string | null): UnitDescription | null {
-  if (!unitName) return null;
-  const units = convert().list();
-  // @ts-ignore
-  const match = units.find(function ({ abbr, singular, plural }) {
-    return (
-      unitName.toLowerCase() === abbr ||
-      unitName.toLowerCase() === singular.toLowerCase() ||
-      unitName.toLowerCase() === plural.toLowerCase() ||
-      unitName.toLowerCase() === translateBritish(singular.toLowerCase())
-    );
-  });
-  return match || null;
+	if (!unitName) return null;
+	const units = convert().list();
+	// @ts-ignore
+	const match = units.find(function ({ abbr, singular, plural }) {
+		return (
+			unitName.toLowerCase() === abbr ||
+			unitName.toLowerCase() === singular.toLowerCase() ||
+			unitName.toLowerCase() === plural.toLowerCase() ||
+			unitName.toLowerCase() === translateBritish(singular.toLowerCase())
+		);
+	});
+	return match || null;
 }
 
 function translateBritish(word: string) {
-  if (word.endsWith('re')) {
-    return word.slice(0, -2) + 'er';
-  }
+	if (word.endsWith('re')) {
+		return word.slice(0, -2) + 'er';
+	}
 }
