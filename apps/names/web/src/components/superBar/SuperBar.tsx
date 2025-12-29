bc3cec0a8940e9f8c43873d0128e7c6ed6dbd4ea
Grant Forrest <gaforres@gmail.com>
2025-12-28 21:50:46 -0500

    [nm] fix z index

--- apps/names/web/src/components/superBar/SuperBar.tsx
+++ apps/names/web/src/components/superBar/SuperBar.tsx
@@ -49,7 +49,7 @@ export function SuperBar({ className }: SuperBarProps) {
 
 const BottomContainer = withClassName(
 	'div',
-	'fixed bottom-0 left-1/2 translate-x--1/2 z-[var(--z-nowPlaying)] md:max-w-512px',
+	'fixed bottom-0 left-1/2 translate-x--1/2 z-[var(--z-now-playing)] md:max-w-512px',
 	'flex flex-col items-stretch justify-center overflow-hidden bg-primary-wash',
 	'border-gray border-solid border-1 lt-sm:(border-b-0 border-l-0 border-r-0) rounded-t-md',
 	'pb-[var(--nav-height,env(safe-area-inset-bottom,0px))]',
