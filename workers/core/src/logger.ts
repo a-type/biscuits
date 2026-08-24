1146fe73b513aa03272a0df4926a351b7f5c05a7
Grant Forrest <gaforres@gmail.com>
2026-08-23 23:19:23 -0400

    stringify object sfor logs

--- workers/core/src/logger.ts
+++ workers/core/src/logger.ts
@@ -1,24 +1,41 @@
 export class Logger {
 	constructor(private tag: string) {}
 
+	// CF observability doesn't print objects, so we need to convert them to strings
+	private processArgs(args: any[]): any[] {
+		return args.map((arg) => {
+			if (arg instanceof Error) {
+				return arg.stack ?? arg.message;
+			}
+			if (typeof arg === 'object') {
+				try {
+					return JSON.stringify(arg);
+				} catch {
+					return String(arg);
+				}
+			}
+			return arg;
+		});
+	}
+
 	info = (...args: any[]) => {
-		console.log(`[${this.tag}]`, ...args);
+		console.log(`[${this.tag}]`, ...this.processArgs(args));
 	};
 
 	warn = (...args: any[]) => {
-		console.warn(`[${this.tag}]`, ...args);
+		console.warn(`[${this.tag}]`, ...this.processArgs(args));
 	};
 
 	urgent = (...args: any[]) => {
-		console.error(`[${this.tag}]`, ...args);
+		console.error(`[${this.tag}]`, ...this.processArgs(args));
 	};
 
 	fatal = (...args: any[]) => {
-		console.error(`[${this.tag}]`, '<<FATAL>>', ...args);
+		console.error(`[${this.tag}]`, '<<FATAL>>', ...this.processArgs(args));
 	};
 
 	debug = (...args: any[]) => {
-		console.debug(`[${this.tag}]`, ...args);
+		console.debug(`[${this.tag}]`, ...this.processArgs(args));
 	};
 }
 
